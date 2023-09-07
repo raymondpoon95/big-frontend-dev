4b358652e899a574b449ae9c8fe18045fc1fd9cf
Raymond Poon <raymondpoon95@hotmail.co.uk>
2023-09-07 17:26:34 +0100

    useDebounce component created

---
+++ src/components/UseDebounce/UseDebounce.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CodeDisplay from "../CodeDisplay/CodeDisplay";
+
+const useDebounceCodeString = `
+
+`;
+
+const UseDebounce = () => {
+  return (
+    <>
+      <h1>UseHover</h1>
+      <CodeDisplay code={useDebounceCodeString} />
+    </>
+  );
+};
+
+export default UseDebounce;
