5c23a2f563ebfd14ae2f4bd45b192ed9f01e3030
Raymond Poon <raymondpoon95@hotmail.co.uk>
2023-09-07 15:26:55 +0100

    refactored useHover solution

--- src/components/UseHover/UseHover.tsx
+++ src/components/UseHover/UseHover.tsx
@@ -2,40 +2,35 @@ import { useEffect, useState, useRef } from "react";
 import CodeDisplay from "../CodeDisplay/CodeDisplay";
 
 const useHoverCodeString = `
-    import { useEffect, useState, useRef } from "react";
+  import { Ref, useState, useRef, useEffect } from 'react'
 
-    const useHover = <T extends HTMLElement>(): [React.RefObject<T>, boolean] => {
-        const ref = useRef<T>(null);
-        const [isHovered, setIsHovered] = useState<boolean>(false);
-      
-        const handleMouseEnter = () => {
-          setIsHovered(true);
-        };
-      
-        const handleMouseLeave = () => {
-          setIsHovered(false);
-        };
-      
-        useEffect(() => {
-          const element = ref.current;
-      
-          if (!element) {
-            return;
-          }
-      
-          element.addEventListener("mouseenter", handleMouseEnter);
-          element.addEventListener("mouseleave", handleMouseLeave);
-      
-          return () => {
-            element.removeEventListener("mouseenter", handleMouseEnter);
-            element.removeEventListener("mouseleave", handleMouseLeave);
-          };
-        }, []);
-      
-        return [ref, isHovered];
-      };
+  export function useHover<T extends HTMLElement>(): [Ref<T>, boolean] {
+    const ref = useRef<T | null>(null);
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+
+    useEffect(() => {
+      setIsHovered(false);
+      const element = ref.current;
+
+      if(!element) return;
+        element.addEventListener('mouseenter', handleMouseEnter);
+        element.addEventListener('mouseleave', handleMouseLeave);
+
+      return () => {
+          element.removeEventListener('mouseenter', handleMouseEnter);
+          element.removeEventListener('mouseleave', handleMouseLeave);
+      }
+    },[ref, isHovered])
+    
+    return [ref, isHovered];
+  }
 `;
 
+// implementation here is a little different because the above solution has a test case that is required to pass on bigfrontend.dev
+// only difference below solution has is that it does not require dependancies in the useEffect
 const useHover = <T extends HTMLElement>(): [React.RefObject<T>, boolean] => {
   const ref = useRef<T>(null);
   const [isHovered, setIsHovered] = useState<boolean>(false);
