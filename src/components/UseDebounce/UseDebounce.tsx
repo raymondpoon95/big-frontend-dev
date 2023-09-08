cbb4b15b73e6ca7a6d0cf1988671dacb1f9af0ef
raymond poon <raymondpoon95@hotmail.co.uk>
2023-09-08 01:44:18 +0100

    useDebounce solution

--- src/components/UseDebounce/UseDebounce.tsx
+++ src/components/UseDebounce/UseDebounce.tsx
@@ -1,8 +1,21 @@
-import React from "react";
 import CodeDisplay from "../CodeDisplay/CodeDisplay";
 
 const useDebounceCodeString = `
+  import {useEffect, useState} from 'react';
 
+  export function useDebounce<T>(value: T, delay: number): T {
+    const[debouncedValue, setDebouncedValue] = useState<T>(value);
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setDebouncedValue(value)
+      },  delay);
+
+      return(() => clearTimeout(timer));
+    },[value, delay]);
+
+    return debouncedValue;
+  }
 `;
 
 const UseDebounce = () => {
